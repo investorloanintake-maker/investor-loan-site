5e23a3f28890ae34c07428678bf30dd16d2fe372
Chad Evers <chadrevers@yahoo.com>
2026-05-26 17:02:57 -0400

    feat: embed YouTube video on insights page with schema, transcript, CTA, compliance

--- deploy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Deploying from: %CD%
-npx wrangler pages deploy . --project-name viador-site
-pause
--- fix-and-push.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d C:\Users\chadr\investor-loan-site
-del /f .git\index.lock 2>nul
-git add -A
-git commit -m "cluster3: dark VP mark on all nav + footer, strip footer wordmark stack (114 nav, 106 footer)"
-git push
-echo DONE
-pause
